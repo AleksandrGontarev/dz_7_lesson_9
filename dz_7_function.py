e6c0e5e8552420b63d4c4f059792b38362ee8ed1
aleksandrgontarev <gontarevsanya@gmail.com>
2022-04-15 16:46:37 +0300

    finish him

--- dz_7_function.py
+++ dz_7_function.py
@@ -20,39 +20,39 @@ print(create_list(my_list))
 #############################################   2
 
 
-def function_find_a(my_listen: list) -> list:
+def get_list_with_forward_symbol_a(in_source: list) -> list:
     """Функция возвращает новый список в котором содержаться элементы
        из my_list у которых первый символ - буква "a"."""
     new_list = []
-    for symbol in my_listen:
+    for symbol in in_source:
         if symbol[0] == "a":
             new_list.append(symbol)
     return new_list
 
 
 my_list = ["qwe", "aty", "uio", "asd"]
-print(function_find_a(my_list))
+print(get_list_with_forward_symbol_a(my_list))
 
 ###########################################   3
 
 
-def function_find_a(my_listen: list) -> list:
+def get_list_with_symbol_a(in_source: list) -> list:
     """Функция возвращает новый список в котором содержаться элементы из my_list
        в которых есть символ - буква "a" на любом месте."""
     new_list = []
-    for symbol in my_listen:
+    for symbol in in_source:
         if symbol.count("a"):
             new_list.append(symbol)
     return new_list
 
 
 my_list = ["qwe", "ray", "uio", "asd"]
-print(function_find_a(my_list))
+print(get_list_with_symbol_a(my_list))
 
 #########################################    4
 
 
-def function_find_str(my_listen: list) -> list:
+def get_str_from_list(my_listen: list) -> list:
     """Функция возвращает новый список в котором содержаться только строки из my_list."""
     new_list = []
     for symbol in my_listen:
@@ -62,7 +62,7 @@ def function_find_str(my_listen: list) -> list:
 
 
 my_list = [1, 2, 3, "11", "22", 33]
-print(function_find_str(my_list))
+print(get_str_from_list(my_list))
 
 #########################################    5
 
@@ -82,7 +82,7 @@ print(find_repeat_letters(my_str))
 
 ##############################################    6
 
-def simbol_find(str_1: str, str_2: str) -> list:
+def get_new_list_with_repeat_symbol(str_1: str, str_2: str) -> list:
     """Функция возвращает список в который поместить те символы,
        которые есть в обеих строках хотя бы раз."""
     return list(set(str_1).intersection(set(str_2)))
@@ -90,24 +90,24 @@ def simbol_find(str_1: str, str_2: str) -> list:
 
 my_str_1 = "akjs"
 my_str_2 = "asko"
-print(simbol_find(my_str_1, my_str_2))
+print(get_new_list_with_repeat_symbol(my_str_1, my_str_2))
 
 ############################################    7
 
 
-def find_simbol(str_1: str, str_2: str) -> list:
+def get_new_list_with_repeat_symbol_once(str_1: str, str_2: str) -> list:
     """ Функция возвращает список в который поместить те символы, которые есть в обеих строках,
         но в каждой только по одному разу"""
     res_list = []
     for symbol in set(str_1).intersection((set(str_2))):
-        if str_1.count(symbol) == 1 and  str_2.count(symbol) == 1:
+        if str_1.count(symbol) == 1 and str_2.count(symbol) == 1:
             res_list.append(symbol)
     return res_list
 
 
 my_str_1 = "aaadtfr"
 my_str_2 = "asdfgh"
-print(find_simbol(my_str_1,my_str_2))
+print(get_new_list_with_repeat_symbol_once(my_str_1, my_str_2))
 
 #############################################     8
 
@@ -115,14 +115,14 @@ import random
 import string
 
 
-def create_email(names: list, domains: list) -> str:
+def create_email(name: list, domain: list) -> str:
     """ функция для генерирования e-mail в формате:
         фамилия.число_от_100_до_999@строка_букв_длинной_от_5_до_7_символов.домен"""
     value_names = random.choice(names)
     value_domains = random.choice(domains)
     value_random_int = random.randint(100, 999)
     value_random_str = ''.join(random.choice(string.ascii_lowercase) for idx in range(0, random.randint(5, 7)))
-    return (f"{value_names}{'.'}{value_random_int}{'@'}{value_random_str}{'.'}{value_domains}")
+    return (f"{value_names}.{value_random_int}@{value_random_str}.{value_domains}")
 
 
 names = ["king", "miller", "kean"]
